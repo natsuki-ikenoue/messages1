d9749d3ddb03c8c9a49cb725445a031f14f6a1c7
natsuki.ikenoue <tsumire617@gmail.com>
2019-08-22 13:08:34 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_111113) do
+ActiveRecord::Schema.define(version: 2019_08_22_125424) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
